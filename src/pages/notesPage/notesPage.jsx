65d299e30269519ca4b7dd87f7516eec3aed46eb
Akndju <davidd.akindoju@gmail.com>
2021-06-22 10:31:56 +0100

    Created small takeNote btn for user friendliness

--- src/pages/notesPage/notesPage.jsx
+++ src/pages/notesPage/notesPage.jsx
@@ -40,7 +40,25 @@ const NotesPage = ({
           <title>arrow-left2</title>
           <path d="M12.586 27.414l-10-10c-0.781-0.781-0.781-2.047 0-2.828l10-10c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-6.586 6.586h19.172c1.105 0 2 0.895 2 2s-0.895 2-2 2h-19.172l6.586 6.586c0.39 0.39 0.586 0.902 0.586 1.414s-0.195 1.024-0.586 1.414c-0.781 0.781-2.047 0.781-2.828 0z"></path>
         </svg>
+
+        {NoteContent.length > 0 && (
+          <Link className="notesPage__addBtn Link" to="/take-note">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <title>add_circle</title>
+              <path d="M17.016 12.984v-1.969h-4.031v-4.031h-1.969v4.031h-4.031v1.969h4.031v4.031h1.969v-4.031h4.031zM12 2.016q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93z"></path>
+            </svg>
+            <p>Take Note</p>
+          </Link>
+        )}
+
         <h1 className="notesPage__title">Notes</h1>
+
         <div className="notesPage__note">
           <SearchBar
             searchValue={searchValue}
