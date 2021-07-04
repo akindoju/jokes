f5671c800eae1cc5efe000e7b64259ea4b0b4575
Akndju <davidd.akindoju@gmail.com>
2021-07-04 15:11:22 +0100

    Fixed notesPage initial styling and added media query

--- src/pages/notesPage/notesPage.jsx
+++ src/pages/notesPage/notesPage.jsx
@@ -36,45 +36,51 @@ const NotesPage = () => {
     <div className="notesPageContainer">
       <Header />
       <div className="notesPage">
-        <svg
-          onClick={(event) => {
-            event.preventDefault();
-            history.goBack();
-          }}
-          className="notesPage__backBtn"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 32 32"
-        >
-          <title>arrow-left2</title>
-          <path d="M12.586 27.414l-10-10c-0.781-0.781-0.781-2.047 0-2.828l10-10c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-6.586 6.586h19.172c1.105 0 2 0.895 2 2s-0.895 2-2 2h-19.172l6.586 6.586c0.39 0.39 0.586 0.902 0.586 1.414s-0.195 1.024-0.586 1.414c-0.781 0.781-2.047 0.781-2.828 0z"></path>
-        </svg>
+        <div className="notesPage__head">
+          <svg
+            onClick={(event) => {
+              event.preventDefault();
+              history.goBack();
+            }}
+            className="notesPage__backBtn"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+          >
+            <title>arrow-left2</title>
+            <path d="M12.586 27.414l-10-10c-0.781-0.781-0.781-2.047 0-2.828l10-10c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-6.586 6.586h19.172c1.105 0 2 0.895 2 2s-0.895 2-2 2h-19.172l6.586 6.586c0.39 0.39 0.586 0.902 0.586 1.414s-0.195 1.024-0.586 1.414c-0.781 0.781-2.047 0.781-2.828 0z"></path>
+          </svg>
 
-        {NoteContent.length > 0 && (
-          <Link className="notesPage__addBtn Link" to="/take-note">
-            <svg
-              onClick={() => {
-                dispatch(setNotesPageGottenTitle(""));
-                dispatch(setNotesPageGottenDate(new Date().toLocaleString()));
-                dispatch(setNotesPageGottenDetails(""));
-                dispatch(setIsNotesPageNoteClicked(false));
-              }}
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <title>add_circle</title>
-              <path d="M17.016 12.984v-1.969h-4.031v-4.031h-1.969v4.031h-4.031v1.969h4.031v4.031h1.969v-4.031h4.031zM12 2.016q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93z"></path>
-            </svg>
-            <p>Take Note</p>
-          </Link>
-        )}
+          <h1 className="notesPage__title">Notes</h1>
 
-        <h1 className="notesPage__title">Notes</h1>
+          {NoteContent.length > 0 && (
+            <Link className=" Link" to="/take-note">
+              <div className="notesPage__addBtn">
+                <svg
+                  onClick={() => {
+                    dispatch(setNotesPageGottenTitle(""));
+                    dispatch(
+                      setNotesPageGottenDate(new Date().toLocaleString())
+                    );
+                    dispatch(setNotesPageGottenDetails(""));
+                    dispatch(setIsNotesPageNoteClicked(false));
+                  }}
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <title>add_circle</title>
+                  <path d="M17.016 12.984v-1.969h-4.031v-4.031h-1.969v4.031h-4.031v1.969h4.031v4.031h1.969v-4.031h4.031zM12 2.016q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93z"></path>
+                </svg>
+                <p>Take Note</p>
+              </div>
+            </Link>
+          )}
+        </div>
 
         <div className="notesPage__note">
           <SearchBar isOnNotesPage={isOnNotesPage.current} />
