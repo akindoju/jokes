64fe76065482b6d4039b8d2717cf45b682e36b82
Akndju <davidd.akindoju@gmail.com>
2021-07-06 13:56:06 +0100

    Added pwa images

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,9 +16,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorker.register();
